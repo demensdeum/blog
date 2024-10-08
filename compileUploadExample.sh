abd7588ef2931252ed6252fe3ec2562476745c01
Ilia Prokhorov <demensdeum@gmail.com>
2024-10-08 11:52:52 +0400

    cryptomus

--- compileUploadExample.sh
+++ compileUploadExample.sh
@@ -1,8 +1,8 @@
-python ./tools/compiler.py --input ./src/robot-defenders.txt --output ./build/robot-defenders.txt
+python ./tools/compiler.py --input ./src/cryptomus-scam.txt --output ./build/cryptomus-scam.txt
 if [ $? -eq 0 ]; then
     echo Compile OK
 else
     echo Compile FAIL
     exit 1
 fi
-python ./tools/uploader.py --post ./build/robot-defenders.txt --blog ./private/wordpress
+python ./tools/uploader.py --post ./build/cryptomus-scam.txt --blog ./private/wordpress
