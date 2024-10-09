616677f6466da7517d455a67e1faa2cec6c63489
Ilia Prokhorov <demensdeum@gmail.com>
2024-10-09 22:18:18 +0400

    Commit

--- compileUploadExample.sh
+++ compileUploadExample.sh
@@ -1,8 +1,8 @@
-python ./tools/compiler.py --input ./src/cryptoBBS.txt --output ./build/cryptoBBS.txt
+python ./tools/compiler.py --input ./src/cryptomus-scam.txt --output ./build/cryptomus-scam.txt
 if [ $? -eq 0 ]; then
     echo Compile OK
 else
     echo Compile FAIL
     exit 1
 fi
-python ./tools/uploader.py --post ./build/cryptoBBS.txt --blog ./private/wordpress
+python ./tools/uploader.py --post ./build/cryptomus-scam.txt --blog ./private/wordpress
